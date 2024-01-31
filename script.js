1b42866fc47b6d33b7291bf0c59d29c965b91028
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2024-01-31 05:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function mapLetters(letter) {
+	let obj = {};
+    let map = new Map();
+    for (let i = 0; i < letter.length; i++) {
+        if (map.has(letter[i])) {
+            map.get(letter[i]).push(i);
+        } else {
+            map.set(letter[i], [i]);
+        }
+    }
+    for (let key of map.keys()) {
+        obj[key] = map.get(key);
+    }
+    return obj;
+}
+
+console.log(mapLetters("dodo"));
+console.log(mapLetters("froggy"));
+console.log(mapLetters("grapes"));
+
